31da5396b2c48f7b7ae462eb07ff727940208e48
Xiaoming Zhou <zhoux@codeaurora.org>
2014-02-20 16:00:53 -0500

    msm: mdss: properly handle panel on and off
    There is some condition that panel off will be called multiple times.
    In this case, it was blindly free the gpios that were already released.
    This is adding the check to handle this condition.
    
    Change-Id: I0140872be1255fe00ed5abd164a246fa57b2a245
    Signed-off-by: Xiaoming Zhou <zhoux@codeaurora.org>
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- drivers/video/msm/mdss/dsi_v2.c
+++ drivers/video/msm/mdss/dsi_v2.c
@@ -94,21 +94,14 @@ static int dsi_panel_handler(struct mdss_panel_data *pdata, int enable)
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
-	if (enable &&
-		(pdata->panel_info.panel_power_state == MDSS_PANEL_POWER_OFF)) {
-		if (!pdata->panel_info.dynamic_switch_pending) {
-			mdss_dsi_panel_reset(pdata, 1);
-			rc = ctrl_pdata->on(pdata);
-			if (rc)
-				pr_err("dsi_panel_handler panel on failed %d\n",
-									rc);
-		}
-		pdata->panel_info.panel_power_state = MDSS_PANEL_POWER_ON;
-		if (pdata->panel_info.type == MIPI_CMD_PANEL)
-			mdss_dsi_set_tear_on(ctrl_pdata);
-	} else if (!enable &&
-		(pdata->panel_info.panel_power_state == MDSS_PANEL_POWER_ON)) {
-		msm_dsi_sw_reset();
+	if (enable && pdata->panel_info.panel_power_on == 0) {
+		dsi_ctrl_gpio_request(ctrl_pdata);
+		mdss_dsi_panel_reset(pdata, 1);
+		pdata->panel_info.panel_power_on = MDSS_PANEL_POWER_ON;
+		rc = ctrl_pdata->on(pdata);
+		if (rc)
+			pr_err("dsi_panel_handler panel on failed %d\n", rc);
+	} else if (!enable && pdata->panel_info.panel_power_on == 1) {
 		if (dsi_intf.op_mode_config)
 			dsi_intf.op_mode_config(DSI_CMD_MODE, pdata);
 		if (pdata->panel_info.dynamic_switch_pending) {
