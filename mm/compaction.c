6e097b894a8130a24980a82fb5c88c1a9ed58ae8
mady51 <root@instance-1.us-east1-b.c.polar-cyclist-235904.internal>
2019-06-06 08:47:30 +0000

    add missing bool variable

--- mm/compaction.c
+++ mm/compaction.c
@@ -1150,6 +1150,7 @@ static int compact_finished(struct zone *zone, struct compact_control *cc,
 	/* Direct compactor: Is a suitable page free? */
 	for (order = cc->order; order < MAX_ORDER; order++) {
 		struct free_area *area = &zone->free_area[order];
+		bool can_steal;
 
 		/* Job done if page is free of the right migratetype */
 		if (!list_empty(&area->free_list[migratetype]))
